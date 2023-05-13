05962f4a8392af4903ad53863c1b26dedc3f0286
marcusdavidalo <marcusdavidalo@gmail.com>
2023-05-13 16:54:16 +0800

    .

--- js/script.js
+++ js/script.js
@@ -100,6 +100,70 @@ if (window.location.href.indexOf('index.html') > -1) {
 }
 
 // ############################################################
+// Movie Details Page
+if (window.location.href.indexOf('movie-details.html') > -1) {
+  const params = new URLSearchParams(window.location.search);
+  const movieId = params.get('id');
+
+  // get movie details from TMDB API
+  const getMovieDetails = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
+    );
+    const data = await response.json();
+    return data;
+  };
+
+  // update movie details in HTML
+  const updateMovieDetails = async () => {
+    const movieDetails = await getMovieDetails();
+    const movieTitle = document.querySelector('#movie-details h2');
+    const movieRating = document.querySelector('#movie-details p');
+    const movieReleaseDate = document.querySelector('#release-date');
+    const moviePoster = document.querySelector('#movie-details img');
+    const movieOverview = document.querySelector(
+      '#movie-details p:last-of-type'
+    );
+    const movieGenres = document.querySelector(
+      '#movie-details .list-group:first-of-type'
+    );
+    const movieBudget = document.querySelector(
+      '.details-bottom ul li:nth-child(1)'
+    );
+    const movieRevenue = document.querySelector(
+      '.details-bottom ul li:nth-child(2)'
+    );
+    const movieRuntime = document.querySelector(
+      '.details-bottom ul li:nth-of-type(3)'
+    );
+    const movieStatus = document.querySelector(
+      '.details-bottom ul li:nth-of-type(4)'
+    );
+    const movieCompanies = document.querySelector(
+      '#movie-details .list-group:last-child'
+    );
+
+    movieTitle.innerText = movieDetails.original_title;
+    moviePoster.src = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
+    movieOverview.innerText = movieDetails.overview;
+    movieGenres.innerHTML = movieDetails.genres
+      .map((genre) => `<li>${genre.name}</li>`)
+      .join('');
+    movieBudget.innerHTML = `<span class="text-secondary">Budget:</span> $${movieDetails.budget.toLocaleString()}`;
+    movieRevenue.innerHTML = `<span class="text-secondary">Revenue:</span> $${movieDetails.revenue.toLocaleString()}`;
+    movieRuntime.innerHTML = `<span class="text-secondary">Runtime:</span> ${movieDetails.runtime} minutes`;
+    movieStatus.innerHTML = `<span class="text-secondary">Status:</span> ${movieDetails.status}`;
+    movieCompanies.innerText = movieDetails.production_companies
+      .map((company) => company.name)
+      .join(', ');
+    movieRating.innerHTML = `<i class="fas fa-star text-primary"></i> ${movieDetails.vote_average} / 10`;
+    movieReleaseDate.innerText = `Release Date: ${movieDetails.release_date}`;
+  };
+
+  updateMovieDetails();
+}
+
+// ############################################################
 // TV Shows Page
 if (window.location.href.indexOf('shows.html') > -1) {
   async function getPopularShows() {
@@ -210,68 +274,4 @@ if (window.location.href.indexOf('search.html') > -1) {
   const term = params.get('search-term');
   const typeMovie = params.get('movie');
   const typeTV = params.get('tv');
-}
-
-// ############################################################
-// Movie Details Page
-if (window.location.href.indexOf('movie-details.html') > -1) {
-  const params = new URLSearchParams(window.location.search);
-  const movieId = params.get('id');
-
-  // get movie details from TMDB API
-  const getMovieDetails = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
-    );
-    const data = await response.json();
-    return data;
-  };
-
-  // update movie details in HTML
-  const updateMovieDetails = async () => {
-    const movieDetails = await getMovieDetails();
-    const movieTitle = document.querySelector('#movie-details h2');
-    const movieRating = document.querySelector('#movie-details p');
-    const movieReleaseDate = document.querySelector('#release-date');
-    const moviePoster = document.querySelector('#movie-details img');
-    const movieOverview = document.querySelector(
-      '#movie-details p:last-of-type'
-    );
-    const movieGenres = document.querySelector(
-      '#movie-details .list-group:first-of-type'
-    );
-    const movieBudget = document.querySelector(
-      '.details-bottom ul li:nth-child(1)'
-    );
-    const movieRevenue = document.querySelector(
-      '.details-bottom ul li:nth-child(2)'
-    );
-    const movieRuntime = document.querySelector(
-      '.details-bottom ul li:nth-of-type(3)'
-    );
-    const movieStatus = document.querySelector(
-      '.details-bottom ul li:nth-of-type(4)'
-    );
-    const movieCompanies = document.querySelector(
-      '#movie-details .list-group:last-child'
-    );
-
-    movieTitle.innerText = movieDetails.original_title;
-    moviePoster.src = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
-    movieOverview.innerText = movieDetails.overview;
-    movieGenres.innerHTML = movieDetails.genres
-      .map((genre) => `<li>${genre.name}</li>`)
-      .join('');
-    movieBudget.innerHTML = `<span class="text-secondary">Budget:</span> $${movieDetails.budget.toLocaleString()}`;
-    movieRevenue.innerHTML = `<span class="text-secondary">Revenue:</span> $${movieDetails.revenue.toLocaleString()}`;
-    movieRuntime.innerHTML = `<span class="text-secondary">Runtime:</span> ${movieDetails.runtime} minutes`;
-    movieStatus.innerHTML = `<span class="text-secondary">Status:</span> ${movieDetails.status}`;
-    movieCompanies.innerText = movieDetails.production_companies
-      .map((company) => company.name)
-      .join(', ');
-    movieRating.innerHTML = `<i class="fas fa-star text-primary"></i> ${movieDetails.vote_average} / 10`;
-    movieReleaseDate.innerText = `Release Date: ${movieDetails.release_date}`;
-  };
-
-  updateMovieDetails();
 }
